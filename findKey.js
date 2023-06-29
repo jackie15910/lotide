c065e2886610ac621d18852dfac0d6e52cb0f341
Jackie <jackie15910@gmail.com>
2023-06-29 19:20:07 -0400

    add and eslinted findKey.js

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, operation) {
+  let allKeys = Object.keys(object);
+  for (let i = 0; i < allKeys.length; i++) {
+    let loopKey = allKeys[i];
+    if (operation(object[loopKey])) {
+      return loopKey;
+    }
+  }
+};
+
+let results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+
+assertEqual(results1, "noma");
