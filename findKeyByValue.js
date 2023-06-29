252b759cb580bee429b3efd346cd94ab67346fc3
Jackie <jackie15910@gmail.com>
2023-06-28 20:59:23 -0400

    Create and added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let allKeys = Object.keys(object);
+  for (let i = 0; i < allKeys.length; i++) {
+    let loopKey = allKeys[i];
+    if (object[loopKey] === value) {
+      return loopKey;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
