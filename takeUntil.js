0f1f83ca2f0dd00332d56427e4b4359037e55c29
Jackie <jackie15910@gmail.com>
2023-06-29 19:19:31 -0400

    Fixes paranthesis

--- takeUntil.js
+++ takeUntil.js
@@ -41,7 +41,7 @@ const results1 = takeUntil(data1, x => x < 0);
 
 /* console.log(results1); */
 
-(assertEqual(results1, [ 1, 2, 5, 7, 2 ]));
+assertEqual(results1, [ 1, 2, 5, 7, 2 ]);
 
 /* const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
